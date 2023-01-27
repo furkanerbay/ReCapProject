60d20240b19e59a3c085ed3e3230c02f16c4ce67
Furkan Erbay <furkanerbay41@gmail.com>
2023-01-27 04:13:00 +0300

    Resim Ekleme

---
+++ Business/Concrete/CarImageManager.cs
@@ -0,0 +1,61 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Helpers.FileHelper;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarImageManager : ICarImageService
+    {
+        private ICarImageDal _carImageDal;
+        private IFileHelper _fileHelper;
+
+        public CarImageManager(ICarImageDal carImageDal, IFileHelper fileHelper)
+        {
+            _carImageDal = carImageDal;
+            _fileHelper = fileHelper;
+        }
+        public IResult Add(IFormFile file, CarImage carImage)
+        {
+            carImage.ImagePath = _fileHelper.Upload(file, PathConstants.ImagesPath);
+            carImage.Date = DateTime.Now;
+            _carImageDal.Add(carImage);
+            return new SuccessResult("Resim basariyla yuklendi.");
+        }
+
+        public IResult Delete(CarImage carImage)
+        {
+            _fileHelper.Delete(PathConstants.ImagesPath + carImage.ImagePath);
+            _carImageDal.Delete(carImage);
+            return new SuccessResult("Resim basari ile silindi.");
+        }
+
+        public IDataResult<List<CarImage>> GetAll()
+        {
+            return new SuccessDataResult<List<CarImage>>(_carImageDal.GetAll(), "Butun araba resimleri donduruldu.");
+        }
+
+        public IDataResult<List<CarImage>> GetByCarId(int carId)
+        {
+            return new SuccessDataResult<List<CarImage>>(_carImageDal.GetAll(c => c.CarId == carId), "Istenilen arabanin resimleri donduruldu");
+        }
+
+        public IDataResult<CarImage> GetByImageId(int imageId)
+        {
+            return new SuccessDataResult<CarImage>(_carImageDal.Get(c => c.Id == imageId));
+        }
+
+        public IResult Update(IFormFile file, CarImage carImage)
+        {
+            carImage.ImagePath = _fileHelper.Update(file, PathConstants.ImagesPath + carImage.ImagePath, PathConstants.ImagesPath);
+            _carImageDal.Update(carImage);
+            return new SuccessResult();
+        }
+    }
+}
---
+++ Core/Utilities/Helpers/FileHelper/FileHelperManager.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Core.Utilities.Helpers.GuidHelperr;
+
+namespace Core.Utilities.Helpers.FileHelper
+{
+    public class FileHelperManager : IFileHelper
+    {
+        public void Delete(string filePath)
+        {
+            if(File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+        }
+
+        public string Update(IFormFile file, string filePath, string root)
+        {
+            if(File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+            return Upload(file,root);
+        }
+
+        public string Upload(IFormFile file, string root)
+        {
+            if(file.Length > 0)
+            {
+                if(Directory.Exists(root))
+                {
+                    Directory.CreateDirectory(root);
+                }
+                string extension = Path.GetExtension(file.FileName);
+                string guid = GuidHelper.CreateGuid();
+                string filePath = guid + extension;
+
+                using (FileStream fileStream = File.Create(root + filePath))//Burada en başta FileStrem class'ının bir örneği oluşturulu., sonrasında File.Create(root + newPath)=>Belirtilen yolda bir dosya oluşturur veya üzerine yazar. (root + newPath)=>Oluşturulacak dosyanın yolu ve adı.
+                {
+                    file.CopyTo(fileStream);//Kopyalanacak dosyanın kopyalanacağı akışı belirtti. yani yukarıda gelen IFromFile türündeki file dosyasınınnereye kopyalacağını söyledik.
+                    fileStream.Flush();//arabellekten siler.
+                    return filePath;//burada dosyamızın tam adını geri gönderiyoruz sebebide sql servere dosya eklenirken adı ile eklenmesi için.
+                }
+            }
+            return null;
+        }
+    }
+}
